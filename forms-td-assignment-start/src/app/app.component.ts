35b3cbf54c085bf13c664a20062d37d6825e134a
Ручкин Олег Аркадьевич <72818824+oruchkin@users.noreply.github.com>
2023-04-22 13:17:08 +0300

    assignment 6 finish templeate forms

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  defaultQuestion = 'advanced';
+  submitted = false;
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  @ViewChild('f', {static: false}) myForm: NgForm;
+  formData = {
+    email: '',
+    password: '',
+    subscription: '',
+  };
 
+  formOnSubmit() {
+    console.log('submitted');
+    console.log(this.myForm);
+    this.submitted = true;
+    this.formData.email = this.myForm.value.email;
+    this.formData.password = this.myForm.value.password;
+    this.formData.subscription = this.myForm.value.subscription;
+  }
 }
