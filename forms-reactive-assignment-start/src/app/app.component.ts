e826f739ab33b7874c45d39ecedafa3d6dca6c6e
Ручкин Олег Аркадьевич <72818824+oruchkin@users.noreply.github.com>
2023-04-26 16:25:40 +0300

    forms recipe start

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+  forbiddenProjects = ['test', 'TEST', 'Test'];
+
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, [Validators.required, this.forbiddenProjectsValidator.bind(this)]),
+      'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmailsAsync),
+      'status': new FormControl('critical'),
+    });
+  }
+
+
+  formOnSubmit() {
+    console.log('submitted');
+    console.log(this.projectForm);
+    console.log('projectName: ' + this.projectForm.value.projectName);
+    console.log('email: ' + this.projectForm.value.email);
+    console.log('status: ' + this.projectForm.value.status);
+  }
+
+  forbiddenProjectsValidator(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenProjects.indexOf(control.value) >= 0) {
+      return {projectIsForbidden: true};
+    }
+    return null;
+  }
+
+
+  forbiddenEmailsAsync(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'oleg@oleg.oleg') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
+
 }
