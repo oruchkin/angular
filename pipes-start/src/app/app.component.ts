c2739b407e0f794e6ff04e5f58b577da537a8b5d
Ручкин Олег Аркадьевич <72818824+oruchkin@users.noreply.github.com>
2023-04-28 17:44:30 +0300

    pipes done

--- pipes-start/src/app/app.component.ts
+++ pipes-start/src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 
+interface Server {
+  instanceType: string;
+  name: string;
+  status: string;
+  started: Date;
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  servers = [
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('stable');
+    }, 2000);
+  });
+  servers: Server[] = [
     {
       instanceType: 'medium',
       name: 'Production Server',
@@ -32,11 +45,24 @@ export class AppComponent {
       started: new Date(15, 1, 2017)
     }
   ];
-  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
+
+  filteredStatus = '';
+
+  getStatusClasses(server: { instanceType: string, name: string, status: string, started: Date }) {
     return {
       'list-group-item-success': server.status === 'stable',
       'list-group-item-warning': server.status === 'offline',
       'list-group-item-danger': server.status === 'critical'
     };
   }
+
+  onAddServer() {
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(15, 1, 2010)
+    });
+  }
+
 }
